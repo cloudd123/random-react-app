55b5f2de542d37b24d848e7a8f71908afd73d05e
cloudd123 <goncalofilipeamro@gmail.com>
2023-07-25 14:32:12 +0100

    upload props one

--- src/components/PokemonCard.jsx
+++ src/components/PokemonCard.jsx
@@ -1,34 +1,22 @@
-function PokemonCard() {
-    const pokemon = pokemonList[1];
-    if (pokemon.name === "bulbasaur") { // Corrected Issue 1
+function PokemonCard(props) {
+    const pokemon = props.pokemonList[0];
+  
+    if (pokemon.name === "bulbasaur") {
       return (
         <figure>
-          <img src={pokemon.imgSrc} alt="bulbasaur" /> 
+          <img src={pokemon.imgSrc} alt="bulbasaur" />
           <figcaption>{pokemon.name}</figcaption>
         </figure>
       );
     } else {
       return (
         <figure>
-            <p>???</p>
-            <figcaption>{pokemon.name}</figcaption>
+          <p>???</p>
+          <figcaption>{pokemon.name}</figcaption>
         </figure>
-        
       );
     }
   }
   
-
-const pokemonList = [
-    {
-      name: "bulbasaur",
-      imgSrc:
-        "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png",
-    },
-    {
-      name: "mew",
-    },
-  ];
-
-
-export default PokemonCard; 
+  export default PokemonCard;
+  
